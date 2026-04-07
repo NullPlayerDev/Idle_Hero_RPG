c9a3806bc3eed948b738527e47ba96347eeeddf2
Mohammad I-K Sakur <mdiftakharkabirsakursparktech@gmail.com>
2026-04-07 11:10:43 +0600

    reward system

---
+++ Assets/Scripts/RewardScripts/RewardCalculator.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class RewardCalculator : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public enum GoldRewardTier()
+    {
+        POOR,
+        NORMAL,
+        RICH,
+        BOSS
+    }85555
+    
+}
