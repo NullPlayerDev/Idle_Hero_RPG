c5240aec4aac855b2cade8b28ba10b93f7971259
Mohammad I-K Sakur <mdiftakharkabirsakursparktech@gmail.com>
2026-05-07 13:17:59 +0600

    tier added and vfx

--- Assets/Scripts/RewardScripts/ChestTiers.cs
+++ Assets/Scripts/RewardScripts/ChestTiers.cs
@@ -1,11 +1,21 @@
 using UnityEngine;
-
+using DG.Tweening;
 public class ChestTiers : MonoBehaviour
 {
+    [SerializeField] private float moveDistance = 15f;
+    [SerializeField] private float duration = 15f;
+    [SerializeField] private ParticleSystem particles;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Destroy(gameObject,3f);
+       particles.Play();
+        transform.DOMoveY(transform.position.y + moveDistance, duration)
+            .SetEase(Ease.OutQuad)
+            .OnComplete(() =>
+            {
+                Destroy(gameObject);
+            });
+        
     }
 
 
