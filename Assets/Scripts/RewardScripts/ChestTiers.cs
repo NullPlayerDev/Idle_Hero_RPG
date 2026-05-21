20c95292c5b356853b58fa273dd089d468111e20
Mohammad I-K Sakur <mdiftakharkabirsakursparktech@gmail.com>
2026-05-21 10:44:35 +0600

    New Update

--- Assets/Scripts/RewardScripts/ChestTiers.cs
+++ Assets/Scripts/RewardScripts/ChestTiers.cs
@@ -4,11 +4,11 @@ public class ChestTiers : MonoBehaviour
 {
     [SerializeField] private float moveDistance = 15f;
     [SerializeField] private float duration = 15f;
-    [SerializeField] private ParticleSystem particles;
+    //[SerializeField] private ParticleSystem particles;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-       particles.Play();
+       //particles.Play();
         transform.DOMoveY(transform.position.y + moveDistance, duration)
             .SetEase(Ease.OutQuad)
             .OnComplete(() =>
