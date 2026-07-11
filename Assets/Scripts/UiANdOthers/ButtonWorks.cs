7ad9f40cdd39b58bd46554e3033d667c5feb7564
Mohammad I-K Sakur <mdiftakharkabirsakur@gmail.com>
2026-07-11 12:55:05 +0600

    updated ui

--- Assets/Scripts/UiANdOthers/ButtonWorks.cs
+++ Assets/Scripts/UiANdOthers/ButtonWorks.cs
@@ -6,7 +6,9 @@ public class ButtonWorks : MonoBehaviour
     [SerializeField] private Button resourceButton;
     [SerializeField] private GameObject resourcePanel;
     [SerializeField] private GameObject playerSelectionPanel;
-
+    [SerializeField] private GameObject[] offPanelList;
+    [SerializeField] private GameObject openPanelList;
+    
     private GameManager gameManager;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -38,5 +40,15 @@ public class ButtonWorks : MonoBehaviour
     {
         Debug.Log("Application exited");
         Application.Quit();
+    }
+
+    public void PanelOpen()
+    {
+        Debug.Log("PanelOpen Called");
+        foreach (var VARIABLE in offPanelList)
+        {
+            VARIABLE.SetActive(false);
+        }
+        openPanelList.SetActive(true);
     }
 }
